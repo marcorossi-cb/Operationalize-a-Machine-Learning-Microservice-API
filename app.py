606d8d52fb5959a7719d2eac6adb6eaaeed4d01e
Marco Rossi <marco.rossi@crispybacon.it>
2020-06-25 17:07:26 +0200

    Update app.py
    fix: change function f string
    
    Removed and replaced function f string because it's gaving an error "W1309: Using an f-string that does not have any interpolated variables (f-string-without-interpolation)"

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
     
-    LOG.info("Scaling Payload: \n"+payload)
+    LOG.info("Scaling Payload: \n%s", payload)
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
@@ -56,9 +56,9 @@ def predict():
     # Logging the input payload
     json_payload = request.json
     #LOG.info(f"JSON payload: \n{json_payload}")
-    LOG.info("JSON payload: \n"+json_payload)
+    LOG.info("JSON payload: \n%s", json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("Inference payload DataFrame: \n"+inference_payload)
+    LOG.info("Inference payload DataFrame: \n%s", inference_payload)
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
